cca72b6c5e50369c93797678a4b47b7177ec2120
ulfitaputri <97174754+ulfitaputri@users.noreply.github.com>
2023-08-13 23:23:00 +0700

    Time: 1769 ms (23.43%), Space: 0B (100.00%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT 
+    EmployeeRanked.department_name as Department,
+    EmployeeRanked.employee_name as Employee,
+    EmployeeRanked.employee_salary as Salary
+FROM (
+    SELECT
+        Department.name as department_name,
+        Employee.name as employee_name,
+        Employee.salary as employee_salary,
+        RANK() OVER (PARTITION BY Employee.departmentId ORDER BY Employee.salary DESC) as salary_rank
+    FROM Employee
+    JOIN Department ON Employee.departmentId = Department.id
+) as EmployeeRanked
+WHERE salary_rank = 1;
+
