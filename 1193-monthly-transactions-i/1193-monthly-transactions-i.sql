561cec3302af6a3c34a59c6a90c7fcfbcb30e23a
ulfitaputri <97174754+ulfitaputri@users.noreply.github.com>
2023-08-22 12:30:45 +0700

    Time: 1319 ms (25.80%), Space: 0B (100.00%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -3,7 +3,7 @@ SELECT
     DATE_FORMAT(trans_date, '%Y-%m') as month,
     country, 
     COUNT(amount) as trans_count,
-    SUM(IF(state = 'approved', 1, 0)) AS approved_count,
+    SUM(state = 'approved') AS approved_count,
     SUM(amount) as trans_total_amount,
     SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
 FROM Transactions
